3601cd5c9a0e83f159c52b2c1a8e852d1661b7ff
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-09-16 09:38:35 +0200

    oppdater dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2022.04.26-13.19-0c38fa7d68db"
+val dittNavDependenciesVersion = "2022.09.13-11.40-2431618c673a"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
