70b863599abe007840b0b80638abfec320722882
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-09-21 15:03:47 +0200

    Revert "fix mismatch i ktor-versjoner fra ditnnav-commmon-lib"
    This reverts commit d6a4ebb691ca9fb0b506b13643b7c08322b6a8a3.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 }
 
 dependencies {
+    implementation(DittNAV.Common.securityAuthenticatedUser)
     implementation(DittNAV.Common.utils)
     implementation(DittNAV.Common.logging)
     implementation(Ktor2.Server.core)
@@ -30,14 +31,17 @@ dependencies {
     implementation(Ktor2.Server.metricsMicrometer)
     implementation(Ktor2.Server.defaultHeaders)
     implementation(Ktor2.Server.cors)
-    implementation("com.github.navikt.tms-ktor-token-support:token-support-tokendings-exchange:2022.08.08-14.36-ac5034ce487b")
-    implementation("no.nav.security:token-validation-ktor-v2:2.1.4")
-    implementation("io.github.microutils:kotlin-logging:2.1.23")
+    implementation("com.github.navikt.tms-ktor-token-support:token-support-tokendings-exchange:2.0.0")
+
 
     implementation(Ktor2.Client.core)
     implementation(Ktor2.Client.apache)
     implementation(Ktor2.Client.contentNegotiation)
     implementation(Ktor2.kotlinX)
+
+
+    implementation("io.github.microutils:kotlin-logging:2.1.23")
+    implementation("com.github.navikt.tms-ktor-token-support:token-support-tokendings-exchange:2022.01.27-13.11-a6b55dd90347")
     implementation(Micrometer.registryPrometheus)
 
     testImplementation(kotlin("test"))
